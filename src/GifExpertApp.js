a1e8aa2e26ed3e58a045752cbde5d154c4fed85d
Alan Juarez <alanwlfx@gmail.com>
2022-02-23 17:13:22 -0600

    ADD: ListCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+    //const categories = ['One punch', 'Samurai X', 'Dragon Ball'] 
+    
+    const [categories, setCategories] = useState(['One punch', 'Samurai X', 'Dragon Ball']);
+
+    const handleAdd = () =>{
+        //setCategories([...categories, 'HunterXHunter']);
+        setCategories(cats => [...categories, 'HunterXHunter']);
+    
+    }
+
+
+
   return (
     <>
         <h1>
             GifExpertApp
         </h1>
-        <hr>
+        <hr />
         
-        </hr>
+        <button onClick={handleAdd}>Agregar</button>
+
+        <ol>
+            {
+                categories.map( category => {
+                    return <li key={ category }> {category} </li>
+                })
+            }
+        </ol>
+
     </>
   )
 }
