77c6c65337486496e78c0e3832900594966cad3c
Alan Juarez <alanwlfx@gmail.com>
2022-02-23 23:29:17 -0600

    ADD: ComponentComunication

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -17,7 +17,7 @@ export const GifExpertApp = () => {
         <h2>
             GifExpertApp
         </h2>
-        <AddCategory />
+        <AddCategory setCategories = { setCategories }/>
         <hr />
         
         
--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
-export const AddCategory = () => {
 
-    const [inputValue, setInputValue] = useState('Wenas')
+export const AddCategory = ( {setCategories} ) => {
+
+    const [inputValue, setInputValue] = useState('');
 
     const handleInputChange = (e) =>{
         
@@ -12,7 +14,11 @@ export const AddCategory = () => {
     const handleSubmit = (e) =>{
         e.preventDefault();
 
-        console.log('Submit hecho')
+        if( inputValue.trim().length > 2 ){
+
+            setCategories( cats => [...cats, inputValue])
+            setInputValue('');
+        }
 
     }
 
@@ -28,4 +34,9 @@ export const AddCategory = () => {
         </form>
         
   )
+}
+
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
 }
