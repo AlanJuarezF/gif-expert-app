b8b10e7a9aec92f9584c815685c8e98abaaf7493
Alan Juarez <alanwlfx@gmail.com>
2022-02-24 00:48:03 -0600

    ADD: Helpers

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { getGif } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ( {category} ) => {
@@ -6,31 +7,10 @@ export const GifGrid = ( {category} ) => {
     const [images, setImages] = useState([]);
 
     useEffect(() => {
-        getGif();
-    }, [])
+        getGif(category)
+            .then(imgs => setImages (imgs))
+    }, [ category ])
 
-    const getGif = async() => {
-
-        const url = 'https://api.giphy.com/v1/gifs/search?q=Valorant&limit=10&api_key=IxtMmBwlYA7T4SfXF6myCpxFyUZ3COZ9'
-        const resp = await fetch( url );
-        const { data } = await resp.json();
-
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url 
-            }
-        })
-
-        console.log(gifs);
-
-        setImages( gifs);
-
-
-    }
-
-    
 
   return (
     <>
